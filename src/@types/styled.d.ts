a81194f0577dfc956ae499b97a00b4950b7d4b22
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-05-27 19:19:27 -0300

    colors and fonts implemented

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import  "styled-components";
+import { defaultTheme } from "../styles/themes/default";
+
+type ThemeType = typeof defaultTheme;
+// usamos para poder ter uma integração melhor com o ts
+declare module 'styled-components' {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    export interface DefaultTheme extends ThemeType{}
+}
