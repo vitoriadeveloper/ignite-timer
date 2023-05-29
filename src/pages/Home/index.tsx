6b885728d2c02ec72d9a3de288959fbe6d69a734
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-05-29 20:04:38 -0300

    home and header implemented

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,55 @@
+import { Play } from "phosphor-react";
+import {
+    CountDownContainer,
+    FormContainer,
+    HomeContainer,
+    MinutesAmountInput,
+    Separator,
+    StartCountdownButton,
+    TaskInput,
+} from "./styles";
+
+export function Home() {
+    return (
+        <HomeContainer>
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput
+                        type="text"
+                        id="task"
+                        placeholder="Dê um nome para o seu projeto"
+                        list="tasks-suggestions"
+                    />
+                    <datalist id="tasks-suggestions">
+                        <option value="" />
+                    </datalist>
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmount"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
+
+                    <span>minutos.</span>
+                </FormContainer>
+                <CountDownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountDownContainer>
+
+                <StartCountdownButton type="submit" disabled>
+                    <Play size={24} />
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    );
+}
